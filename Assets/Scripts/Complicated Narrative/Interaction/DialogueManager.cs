5b3ab0df00aada845a132a8fbba00a00e2eb9ed5
amrnator <amrsaleh129@gmail.com>
2017-09-11 19:08:06 -0400

    Tweaked Dialogue box
    Replaced test in Dialogue box with textmeshpro text objects, and added animations and bug fixes. Additional tweaking is necessary later

--- Assets/Scripts/Complicated Narrative/Interaction/DialogueManager.cs
+++ Assets/Scripts/Complicated Narrative/Interaction/DialogueManager.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour {
 
 	public Queue<string> sentences;
 
-	public Text nameText;
+	public TextMeshProUGUI nameText;
 
-	public Text dialogueText;
+	public TextMeshProUGUI dialogueText;
+
+	public Animator animator;
 
 	void Start () {
 		sentences = new Queue<string> ();
@@ -17,6 +20,8 @@ public class DialogueManager : MonoBehaviour {
 
 	public void StartDialogue(Dialogue dialog)
 	{
+		animator.SetBool ("isOpen", true);
+
 		print ("Starting Convo with: " + dialog.name);
 
 		nameText.text = dialog.name;
@@ -33,15 +38,33 @@ public class DialogueManager : MonoBehaviour {
 	public void displayNextSentence(){
 
 		if (sentences.Count == 0) {
-			EndDialogue();	
+			EndDialogue();
+			return;
 		}
 
 		string sentence = sentences.Dequeue ();
 
-		dialogueText.text = sentence;
+		StopAllCoroutines ();
+
+		StartCoroutine (typeSentence(sentence));
 	}
 
+	IEnumerator typeSentence(string sentence){
+		dialogueText.text = "";
+
+		foreach (char letter in sentence.ToCharArray()) {
+			dialogueText.text += letter;
+
+			//wait two frames
+			yield return null;
+			yield return null;
+		}
+	}
+
+
 	void EndDialogue(){
+		animator.SetBool ("isOpen", false);
+
 		print ("Convo over");
 	}
 }
