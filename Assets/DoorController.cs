34437894496a8b672b7657590b70bb79006c58a2
amrnator <amrsaleh129@gmail.com>
2017-09-17 22:14:10 -0400

    Added final tweaks
    added fading text in log, some more small adjustments are neccesary but wont warrant its own commit

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 public class DoorController : MonoBehaviour {
 
     public void openDoor() {
-        print("Door Opening");
+		GameLog.Instance.post("Door Opening");
 
         transform.position = new Vector3(transform.position.x, transform.position.y - 10, transform.position.z);
     }
